3fc16b43fbc6356c029b5ac88a898e895465e9e0
hoai97nam <nhnam97@gmail.com>
2021-03-13 21:58:38 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-st6kve
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-st6kve)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+
+import { products } from "../products";
+
+@Component({
+  selector: "app-product-list",
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"]
+})
+export class ProductListComponent {
+  products = products;
+
+  share(name: string) {
+    window.alert("The product has been shared!\n" + name);
+  }
+  onNotify() {
+    window.alert("doume, u click 4 what ?");
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
